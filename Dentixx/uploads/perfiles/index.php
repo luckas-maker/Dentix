ad00eb0a43ba9ed842a6eaa32be6b4bd22e526c8
luckas-maker <dragon15luis@gmail.com>
2025-11-12 21:10:59 -0600

    Delete Dentixx directory

--- Dentixx/uploads/perfiles/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Archivo de seguridad para carpeta de perfiles
-header('HTTP/1.0 403 Forbidden');
-header('Content-Type: text/plain; charset=utf-8');
-exit('Acceso denegado - Área restringida');
-?>
